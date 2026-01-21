558c2f31064ca4247a48353eec303678c3976963
preethamss60 <preethamss.sigdhal@gmail.com>
2026-01-21 20:32:58 +0530

    responsive

--- src/pages/coreteam.tsx
+++ src/pages/coreteam.tsx
@@ -10,6 +10,7 @@ const teamMembers = [
   { imageSrc: '/chill.jpg', title: 'Adithya SN', subtitle: 'Treasurer' },
   { imageSrc: '/chill.jpg', title: 'Adithya SN', subtitle: 'Event Coordinator' },
   { imageSrc: '/chill.jpg', title: 'Adithya SN', subtitle: 'Tech Lead' },
+  { imageSrc: '/chill.jpg', title: 'Adithya SN', subtitle: 'Tech Lead' },
 ];
 
 export default function CoreTeamPage() {
@@ -53,7 +54,7 @@ export default function CoreTeamPage() {
         </motion.h1>
 
         {/* Team Members Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-16 mt-16 w-full max-w-6xl justify-items-center">
+        <div className="flex flex-wrap  gap-16 mt-16 w-full max-w-6xl justify-center">
           {teamMembers.map((member, index) => (
             <CoreTeamCard
               key={index}
--- src/pages/techteam.tsx
+++ src/pages/techteam.tsx
@@ -140,7 +140,7 @@ export default function TechTeamPage() {
         <div className="relative pt-20 mt-10 flex flex-col items-center justify-center w-full">
         {/* Animated Title - Fade Up */}
           <motion.h1
-            className="font-['Michroma'] text-4xl sm:text-5xl sm:top-13 md:top-11 lg:top-4  top-16 text-center md:text-6xl absolute lg:text-8xl font-bold w-full mt-12 bg-gradient-to-b from-white via-white to-transparent bg-clip-text text-transparent tracking-wider"
+            className="font-['Michroma'] text-3xl sm:text-5xl sm:top-13 md:top-11 lg:top-4  top-17 text-center md:text-6xl absolute lg:text-8xl font-bold w-full mt-12 bg-gradient-to-b from-white via-white to-transparent bg-clip-text text-transparent tracking-wider"
             initial={{ opacity: 0, y: 80 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{
@@ -169,7 +169,7 @@ export default function TechTeamPage() {
         <div className="relative pt-20 mt-20 flex flex-col items-center justify-center w-full">
         {/* Animated Title - Fade Up */}
           <motion.h1
-            className="font-['Michroma'] text-4xl sm:text-5xl sm:top-13 md:top-11 lg:top-4  top-16 text-center md:text-6xl absolute lg:text-8xl font-bold w-full mt-12 bg-gradient-to-b from-white via-white to-transparent bg-clip-text text-transparent tracking-wider"
+            className="font-['Michroma'] text-3xl sm:text-5xl sm:top-13 md:top-11 lg:top-4  top-17 text-center md:text-6xl absolute lg:text-8xl font-bold w-full mt-12 bg-gradient-to-b from-white via-white to-transparent bg-clip-text text-transparent tracking-wider"
             initial={{ opacity: 0, y: 80 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{
@@ -198,7 +198,7 @@ export default function TechTeamPage() {
         <div className="relative pt-20 mt-20 flex flex-col items-center justify-center w-full">
         {/* Animated Title - Fade Up */}
           <motion.h1
-            className="font-['Michroma'] text-4xl sm:text-5xl sm:top-13 md:top-11 lg:top-4  top-16 text-center md:text-6xl absolute lg:text-8xl font-bold w-full mt-12 bg-gradient-to-b from-white via-white to-transparent bg-clip-text text-transparent tracking-wider"
+            className="font-['Michroma'] text-3xl sm:text-5xl sm:top-13 md:top-11 lg:top-4  top-17 text-center md:text-6xl absolute lg:text-8xl font-bold w-full mt-12 bg-gradient-to-b from-white via-white to-transparent bg-clip-text text-transparent tracking-wider"
             initial={{ opacity: 0, y: 80 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{
