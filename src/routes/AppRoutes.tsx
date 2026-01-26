aba99db13f2fbf75f1f8974db0acf9af90a28d9c
Shishir Karkera <dn.shishirgk@gmail.com>
2026-01-26 11:07:22 +0530

    feat: Implement parallax landing page with new assets, animations, and global styles.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -44,8 +44,8 @@ const ResetRedirect = () => {
 function AppRoutes() {
   return (
     <Routes>
+    <Route path="/" element={<HomePage />} />
       <Route element={<Layout />}>
-        <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/events" element={<EventsPage />} />
